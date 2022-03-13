cb95b179add7c2d970622bb0eba838effdef76d9
Anish Gowda <anishgowda43@gmail.com>
2022-03-13 06:09:24 +0530

    added nyaa support

--- app.js
+++ app.js
@@ -1,63 +1,79 @@
-const express = require('express');
-const udemy = require('./udemy.js');
-const yts = require('./yts.js');
-const adike = require('./adike.js');
+const express = require("express");
+const udemy = require("./udemy.js");
+const yts = require("./yts.js");
+const adike = require("./adike.js");
+const nyaa = require("./nyaa.js");
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/udemy',(req,res)=>{
-    (async() =>{
-        try{
-            var data = await udemy(req.query.query);
-            res.setHeader('content-type', 'application/json');
-            res.status(200).json(data);
-        }catch(err){
-            res.send(err);
-        }
-    })()
+app.get("/udemy", (req, res) => {
+  (async () => {
+    try {
+      var data = await udemy(req.query.query);
+      res.setHeader("content-type", "application/json");
+      res.status(200).json(data);
+    } catch (err) {
+      res.send(err);
+    }
+  })();
 });
 
-app.get('/yts',(req,res)=>{
-    if (req.query.query === undefined){
-        res.end('Please provide a query\nExample: /yts?query=hulk');
-    }
-    else{
-        console.log(req.query.query);
-    (async()=>{
-        try{
-            var data = await yts(req.query.query);
-            res.setHeader('content-type', 'application/json');
-            res.status(200).json(data);
-        }catch(err){
-            res.send(err);
-        }
-    })()
-    }
+app.get("/yts", (req, res) => {
+  if (req.query.query === undefined) {
+    res.end("Please provide a query\nExample: /yts?query=hulk");
+  } else {
+    console.log(req.query.query);
+    (async () => {
+      try {
+        var data = await yts(req.query.query);
+        res.setHeader("content-type", "application/json");
+        res.status(200).json(data);
+      } catch (err) {
+        res.send(err);
+      }
+    })();
+  }
 });
 
-app.get("/adike",(req,res)=>{
-    if (req.query.city_id === undefined || req.query.date === undefined){
-        res.end('Please provide a city_id and date\nExample: /adike?city_id=3&date=2021-11');
-    }
-    else{
-        console.log(req.query.city_id,req.query.date);
-    (async()=>{
-        try{
-            var data = await adike(req.query.city_id,req.query.date);
-            res.setHeader('content-type', 'application/json');
-            res.status(200).json(data);
-        }catch(err){
-            res.send(err);
-        }
-    })()
-    }
+app.get("/adike", (req, res) => {
+  if (req.query.city_id === undefined || req.query.date === undefined) {
+    res.end(
+      "Please provide a city_id and date\nExample: /adike?city_id=3&date=2021-11"
+    );
+  } else {
+    console.log(req.query.city_id, req.query.date);
+    (async () => {
+      try {
+        var data = await adike(req.query.city_id, req.query.date);
+        res.setHeader("content-type", "application/json");
+        res.status(200).json(data);
+      } catch (err) {
+        res.send(err);
+      }
+    })();
+  }
 });
 
-
+app.get("/nyaa", (req, res) => {
+  if (req.query.query === undefined) {
+    res.end("Please provide a query\nExample: /nyaa?query=naruto");
+  } else {
+    var ob = req.query.ord_by === "date" ? "id" : "seeders";
+    var p = req.query.page || 1;
+    (async () => {
+      try {
+        var data = await nyaa(req.query.query, ob, p);
+        res.setHeader("content-type", "application/json");
+        res.status(200).json(data);
+      } catch (err) {
+        res.send(err);
+      }
+    })();
+  }
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port , () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ nyaa.js
@@ -0,0 +1,58 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
+
+// https://github.com/Ryuk-me/Torrents-Api/torrent/nyaaSI.js#L15
+
+const nameRegex = /[a-zA-Z\W].+/g;
+
+async function getNyaaData(query, ord_by, page) {
+  try {
+    var baseUrl = `https://nyaa.si/?q=${query}&s=${ord_by}&o=desc&page=${page}`;
+    const res = await axios.get(baseUrl);
+    const $ = cheerio.load(res.data);
+    const nyaaPromises = $("table > tbody > tr")
+      .toArray()
+      .map(async (el, i) => {
+        const tds = $(el).find("td");
+        const type = $(tds[0]).find("a").find("img").attr("alt");
+        const name = $(tds[1]).text().trim().match(nameRegex)[0].trim();
+        const torrent =
+          "https://nyaa.si" + $(tds[2]).find("a").first().attr("href");
+        const magnet = $(tds[2]).find("a").last().attr("href");
+        const size = $(tds[3]).text();
+        const upload_date = $(tds[4]).text().trim();
+        const seeders = $(tds[5]).text().trim();
+        const leechers = $(tds[6]).text().trim();
+        const complete_downloads = $(tds[7]).text().trim();
+
+        var dataObj = {
+          type,
+          name,
+          torrent,
+          magnet,
+          size,
+          upload_date,
+          seeders,
+          leechers,
+          complete_downloads,
+        };
+        return dataObj;
+      });
+    const finalArray = await Promise.all(nyaaPromises);
+    finalObj = {
+      status: "success",
+      data: finalArray,
+    };
+    return finalObj;
+  } catch (error) {
+    return {
+      status: "error",
+      error_code: error.code,
+      error_message: error.message,
+      error_stack: error.stack,
+    };
+  }
+}
+
+module.exports = getNyaaData;
