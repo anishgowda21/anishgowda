b5fd3b9c7f5c641b1122fae37246bbb7e50eb672
Anish Gowda <anishgowda43@gmail.com>
2025-03-01 12:45:08 +0530

    test methods 1 to overcome 1337x block to vercel

--- 1337x.js
+++ 1337x.js
@@ -41,6 +41,10 @@ async function search(
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        Referer: "https://1337x.to/",
       },
     });
 
